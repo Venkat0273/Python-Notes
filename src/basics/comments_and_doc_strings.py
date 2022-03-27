a72e2bf0c48676f955e1b0225d14a48a2128d6e9
Venkatarami Reddy <venkataram0273@gmail.com>
2022-03-27 22:18:46 +0530

    Added types and comments files

---
+++ src/basics/comments_and_doc_strings.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+__author__ = "venkat"
+__author_email__ = "venkatram0273@gmail.com"
+
+
+# def comments_and_doc_strings() -> None:
+#     """
+#     These are multi-line comments.
+#     Used to document specific information about the function.
+#     These are called docstrings as well and can be accessed through func.__doc__
+#     Arguments:
+#     None: None
+#     Return:
+#     None
+#     """
+#     # This is single line comment used to highlight that is important logic to understand
+#     a: int = 23 # This is inline comment used to elaborate more about this specific line
+#     print(a)
+
+
+# print(comments_and_doc_strings.__doc__)
+
+# def google_style_guide(name: str, language: str = "en") -> int:
+#     """Say hello to a person
+#     Args:
+#         name: Name of the person to say hello
+#         language: Language in which the person wants greeting to be printed
+#     Returns:
+#         A number
+#     """
+#     print(" ".join(["Hello", name]))
+#     return 4
+
+
+# google_style_guide(name="venkat")
+# print(google_style_guide.__doc__)
---
+++ src/basics/types.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+__author__ = "venkat"
+__author_email__ = "venkatram0273@gmail.com"
+
+
+from typing import List
+
+
+# def greeting(name: str) -> str:
+#     return " ".join(["Hello", name])
+
+
+# print(greeting("venkat"))
+
+# Vector = List[float]
+
+# def scale(scalar: float, vector: Vector) -> Vector:
+#     return [scalar * num for num in vector]
+
+
+# print(scale(2.0, [1.0, -4.2, 4.3]))
+
+
